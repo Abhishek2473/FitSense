5b9f370edb8ac4d68817b7f9e9dd105942f270a4
Abhishek Reddy <abhishekreddy2407@gmail.com>
2025-02-10 00:11:46 +0530

    final

--- api_key.py
+++ api_key.py
@@ -1,2 +1,1 @@
-API_KEY = "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"
-#  $env:GROQ_API_KEY="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"
+API_KEY = "insert your api key"
